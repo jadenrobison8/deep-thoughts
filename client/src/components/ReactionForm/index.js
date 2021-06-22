648216ba3892ce5958524880303ff3977234def7
robisonja <jadenrobison8@gmail.com>
2021-06-21 21:05:31 -0600

    changed reaction form

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_REACTION } from '../../utils/mutations';
+
 const ReactionForm = ({ thoughtId }) => {
    const [reactionBody, setBody] = useState('');
    const [characterCount, setCharacterCount] = useState(0);
